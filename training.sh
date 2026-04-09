d10a97cf3358909208bf6dfed73a08ac9bdc72db
Alexander Müller <a.k.muller.2@student.rug.nl>
2026-04-09 11:53:06 +0200

    new results

--- training.sh
+++ training.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=training_job
-#SBATCH --time=12:00:00
+#SBATCH --time=03:00:00
 #SBATCH --mem=12GB
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
 cd $HOME/MoLe
-mole-train --dataset esol
-mole-train --dataset hce
-mole-train --dataset qm9
+python mole/scripts/training.py
